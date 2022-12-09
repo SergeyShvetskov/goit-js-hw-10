ac321d9c9f4229fe362f96991d2a1ed968841cb3
User61 <svetsoff@gmail.com>
2022-12-09 15:00:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,59 +8,63 @@ const DEBOUNCE_DELAY = 300;
 const URL = 'https://restcountries.com/v3.1/name/';
 
 const refs = {
-    list: document.querySelector('.news-list'),
-    submitButton: document.querySelector('.news-submit'),
-    loader: document.querySelector('.news-loader'),
-    inputText: document.querySelector('input#search-box'),
-}
+  list: document.querySelector('.news-list'),
+  submitButton: document.querySelector('.news-submit'),
+  loader: document.querySelector('.news-loader'),
+  inputText: document.querySelector('input#search-box'),
+};
 
 let items = [];
 
 refs.inputText.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(params) {
-    params.preventDefault;
-    const value = params.target.value;
-    let valueTrim = value.trim();
-    if (valueTrim) {
-    refs.list.innerHTML = "";
-        showLoader();
-        // fetchCountries(valueTrim);
+  params.preventDefault;
+  const value = params.target.value;
+  let valueTrim = value.trim();
+  if (valueTrim) {
+    refs.list.innerHTML = '';
+    showLoader();
+    // fetchCountries(valueTrim);
 
-         fetch(`${URL}${valueTrim}?fields=name,capital,population,flags,languages`)
-        .then(resp => {
-            if (!resp.ok) {
-      throw Error();
-            } 
-           return resp.json();
-        })
-        .then(data => {
-            items = data;
-            console.log(items);
-            render();
-        })
-        .catch(error => {
-            Notiflix.Notify.failure("Oops, there is no country with that name");
-        })
-        .finally(() => {
-            hideLoader();
-        });
-    }
+    fetch(`${URL}${valueTrim}?fields=name,capital,population,flags,languages`)
+      .then(resp => {
+        if (!resp.ok) {
+          throw Error();
+        }
+        return resp.json();
+      })
+      .then(data => {
+        items = data;
+        console.log(items);
+        render();
+      })
+      .catch(error => {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      })
+      .finally(() => {
+        hideLoader();
+      });
+  }
 }
 
-
-
-const getItemtemplateMin = ({name, flags }) => {
-    let result = `<li class="news-list-li"> 
+const getItemtemplateMin = ({ name, flags }) => {
+  let result = `<li class="news-list-li"> 
     <img width = 30px src=${flags.svg}>
     <span class="name-country"> ${name.official}</span>
     </li>`;
-    return result;
-}
+  return result;
+};
 
-const getItemtemplateMax = ({name, capital, population, flags, languages}) => {
-    let lang = Object.values(languages).join(", ");
-    let result = `<li class="news-item">
+const getItemtemplateMax = ({
+  name,
+  capital,
+  population,
+  flags,
+  languages,
+}) => {
+  let lang = Object.values(languages).join(', ');
+  let result = `<li class="news-item">
        <img width = 30px src=${flags.svg} alt=${name}> 
     <span class="name-country-big"> ${name.official}</span>
       <p><b>Capital:</b> ${capital}</p>
@@ -68,29 +72,29 @@ const getItemtemplateMax = ({name, capital, population, flags, languages}) => {
       <p><b>Languages:</b> ${lang}</p>
         </li>
     `;
-    return result;
-    };
+  return result;
+};
 
 const render = () => {
-    if (items.length > 10) {
-        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-    } else if (items.length <= 10 && items.length >= 2) { 
-        const list = items.map(getItemtemplateMin);
-        refs.list.innerHTML = "";
-        refs.list.insertAdjacentHTML('beforeend', list.join(''));
-    } else if (items.length == 1 ){
-        const list = items.map(getItemtemplateMax);
-        refs.list.innerHTML = "";
-        refs.list.insertAdjacentHTML('beforeend', list.join(''));
-    }
+  if (items.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  } else if (items.length <= 10 && items.length >= 2) {
+    const list = items.map(getItemtemplateMin);
+    refs.list.innerHTML = '';
+    refs.list.insertAdjacentHTML('beforeend', list.join(''));
+  } else if (items.length == 1) {
+    const list = items.map(getItemtemplateMax);
+    refs.list.innerHTML = '';
+    refs.list.insertAdjacentHTML('beforeend', list.join(''));
+  }
 };
 
 const showLoader = () => {
-    refs.loader.classList.add('show');
+  refs.loader.classList.add('show');
 };
-
 
 const hideLoader = () => {
-    refs.loader.classList.remove('show');
+  refs.loader.classList.remove('show');
 };
-
